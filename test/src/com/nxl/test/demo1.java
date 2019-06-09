a8214cd5b28104f05cad4d14455eff8924d3f79c
nxl <102202484@qq.com>
2019-06-09 10:57:53 +0800

    第一次提交

---
+++ test/src/com/nxl/test/demo1.java
@@ -0,0 +1,16 @@
+package com.nxl.test;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("测试");
+
+
+        System.out.println("测试");
+        System.out.println("测试");
+        System.out.println("测试");
+
+        System.out.println(111111);
+
+
+    }
+}
